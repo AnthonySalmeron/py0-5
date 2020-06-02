ebef64e570fd0ee28018b349acf4ba4a34cb23a5
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-06-01 20:42:13 -0400

    first commit

---
+++ ps0.py
@@ -0,0 +1,5 @@
+from math import log2 as log
+x = int(input('Input base: '))
+y = int(input('Input power: '))
+print(x**y)
+print(log(x))
---
+++ ps1.py
@@ -0,0 +1,7 @@
+total_cost = float(input('What is the cost of your dream house'))
+portion_down_payment = 0.25 * total_cost
+current_savings​ = 0
+r = 0.04
+annual_salary =
+portion_saved =
+current_savings += 
