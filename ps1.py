9f2bbb2d89fa908019b2babcb1db723815bc6354
Anthony Salmeron <anthonysalmerondev@gmail.com>
2020-06-04 20:21:24 -0400

    finished

--- ps1.py
+++ ps1.py
@@ -19,49 +19,43 @@
 import math
 # Part C
 def myFunc(portion,annual_salary):
-    total_cost = 1000000
     semi_annual_raise = 0.07
-    portion_saved = portion/100000
-    portion_down_payment = 0.25 * total_cost
+    portion_saved = portion/10000
     current_savings = 0
     monthly_salary = annual_salary/12
     r = 0.04
-    months_to_pay_down_payment = 0
+    months = 0
 
-    while(current_savings<(portion_down_payment-100)):
-        months_to_pay_down_payment += 1
+    while(months<36):
+        months += 1
         current_savings += current_savings * r/12
         current_savings += portion_saved * monthly_salary
-        if(current_savings>=portion_down_payment and current_savings < portion_down_payment+100):
-            return months_to_pay_down_payment
-        if(months_to_pay_down_payment%6 == 0):
+        if(months%6 == 0):
             monthly_salary+= monthly_salary * semi_annual_raise
-    return months_to_pay_down_payment
+    return current_savings
 
 annual_salary = float(input("What is your annual salary: "))
-goal = 36
+goal = 250000
 current = 0
 min = 1
 max = 10000
 iterations = 0
 while(max-min > 2):
-    print(iterations)
     iterations+=1
     options = max-min +1
     lower = min + math.floor(options/2) - 1
     upper = lower + 1
     upp = myFunc(upper,annual_salary)
     low = myFunc(lower,annual_salary)
-    print(upp,low) # currently getting months value from myfunc but why not instead make it so get total after three years
-    if(upp==goal):
+    if((upp>=goal-100) and (upp<=goal+100)):
         current = 1
         print("iterations: ", iterations)
-        print("percent to save: ", upper/100)
+        print("percent to save: ", upper/10000)
         break
-    if(low==goal):
+    if((low>=goal-100) and (low<=goal+100)):
         current = 1
         print("iterations: ", iterations)
-        print("percent to save: ", lower/100)
+        print("percent to save: ", lower/10000)
         break
     if(abs(goal-upp)<abs(goal-low)):
          min = upper
@@ -72,7 +66,7 @@ if(max-min<3 and current == 0):
         ret = myFunc(i,annual_salary)
         if(ret==goal):
             print("iterations: ", iterations+1)
-            print("percent to save: ", i/100)
+            print("percent to save: ", i/10000)
             break
 if(current == 0):
     print('no savings rate can get you there')
